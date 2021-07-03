954567f15b995f99ae04f1c6cc09bdde9a65dfd0
hooyn <hooyn_industry@naver.com>
2021-07-03 23:52:04 +0900

    teamsb server setting

--- router/join.js
+++ router/join.js
@@ -26,6 +26,11 @@ router.get('/', function(req, res){
     res.render('join.ejs', {'message' : msg});
 });
 
+passport.serializeUser(function(user, done){
+    console.log('passport session save : ', user.id)
+    done(null, user.id) //line49에서 user에게 넘겨준 값을 
+})
+
 passport.use('local-join', new LocalStrategy({
     usernameField: 'id',
     passwordField: 'password',
@@ -34,11 +39,16 @@ passport.use('local-join', new LocalStrategy({
     var query = connection.query('select * from users where id=?', [id], function(err, rows){
         if(err) return done(err);
         
-        if(rows.length){
+        if(rows.length){ //이미 아이디가 있다면 이미 있다는 메세지와 함께 err
             console.log('existed user');
             return done(null, false, {message : 'your id is already used'})
         } else {
-            
+            var sql = {id:id, password:password};
+            var query = connection.query('insert into users set ?', sql, function(err, rows){
+                if(err) throw err
+                return done(null, {'id':id, 'password':password});
+                //세션에 담을 정보를 넘겨준다. user에게 담아서 serialize에게 전달
+            })
         }
     })
 }));
