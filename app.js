9544a1b5ce64b7ec44f5dc82dbadea957b30ffc6
hooyn_n <66655076+hooyn@users.noreply.github.com>
2021-08-19 02:27:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
 var app = express();
+const admin = require('firebase-admin')
 var bodyParser = require('body-parser'); //post 형태의 data 받기 위해서 필요한 모듈
 var passport = require('passport');
 var session = require('express-session');
 var flash = require('connect-flash');
+var dateFormat = require('dateformat');
 var router = require('./router/index');
-var func = require('./function');
-
-func.handleDisconnect();
 
 app.listen(3000, function(){
-    console.log('server3000 running');
+    console.log('server3000 running' + " [ " + dateFormat(Date(), "yyyy-mm-dd, h:MM:ss TT") + " ] " );
 });
 
 app.use(express.static('public'));
 app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
+app.use(bodyParser.urlencoded({
+    limit: '5mb',
+    extended:false
+}));
 
 app.use(session({
     secret: 'keyboard cat',
@@ -30,8 +34,9 @@ app.use(flash());
 
 app.use(router);
 
-
-
-
-
+//SDK initialize
+let serAccount = require('./key/teamsb-68280-firebase-adminsdk-n930z-2636739ff1.json') //fcm 서버 키
+admin.initializeApp({
+credential: admin.credential.cert(serAccount),
+})
 
