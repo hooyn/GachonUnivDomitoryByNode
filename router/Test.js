8c906824428f4813db9e7501f021ce0f8a6cae67
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-01-05 07:17:34 +0900

    Update Test.js

--- router/Test.js
+++ router/Test.js
@@ -8,11 +8,11 @@ const { response } = require('express');
 
 //DATABASE SETTING
 var teamsbDB = {
-  host : '13.209.10.30',
+  host : '**',
   port : 3306,
   user : 'root',
-  password : 'owner9809~',
-  database : 'teamsb',
+  password : '**',
+  database : '**',
   dateStrings : 'date'
 };
 var connection
